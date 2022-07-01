052ef25b8b6cd886870a2877926efaf789ee11c5
Shanmugasundaram Raja <100921063+ShanmugasundaramRaja@users.noreply.github.com>
2022-07-01 15:57:59 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './components/Home';
+import MyNavbar from './components/MyNavbar';
+
+function App()
+{
+  return (
+    <div>
+      <MyNavbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
